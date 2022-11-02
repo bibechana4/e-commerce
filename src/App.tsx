2dfaeabcb9645a8b81b61e490cb72b5766c5ceb9
bibechana4 <adhikaribibechana4@gmail.com>
2022-11-02 09:46:07 -0500

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/:productId">Product details</Route>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
